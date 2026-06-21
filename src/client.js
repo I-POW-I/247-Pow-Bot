bc7d4d04ebba5bdc0f20d0e7c5312de62ca98b39
POW <146384484+I-POW-I@users.noreply.github.com>
2026-06-21 23:29:34 +0100

    Update client.js

--- src/client.js
+++ src/client.js
@@ -4,7 +4,9 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildMembers,   // Needed for member info in voice logs
+    GatewayIntentBits.GuildMembers,   // Member info in voice logs
+    GatewayIntentBits.GuildMessages,  // Message delete events
+    GatewayIntentBits.MessageContent, // Read deleted message content (privileged intent)
   ],
 });
 
