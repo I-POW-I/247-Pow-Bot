b29b61e9f18c71bf2c1f71c7410f2af572897121
POW <146384484+I-POW-I@users.noreply.github.com>
2026-07-14 23:30:27 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
     .setName('ping')
     .setDescription("Check the bot's response time"),
   async execute(interaction, client) {
-    const sent  = await interaction.reply({ content: '🏓 Measuring...', flags: [MessageFlags.Ephemeral], fetchReply: true });
+    const sent  = await interaction.reply({ content: 'Checking...', flags: [MessageFlags.Ephemeral], fetchReply: true });
     const round = sent.createdTimestamp - interaction.createdTimestamp;
     const ws    = client.ws.ping;
     await interaction.editReply({
@@ -13,7 +13,7 @@ module.exports = {
       embeds: [
         new EmbedBuilder()
           .setColor(ws < 100 ? 0x57F287 : ws < 200 ? 0xFEE75C : 0xED4245)
-          .setTitle('🏓 Pong')
+          .setTitle('Pong')
           .addFields(
             { name: 'Roundtrip', value: `${round}ms`, inline: true },
             { name: 'WebSocket', value: `${ws}ms`,    inline: true },
